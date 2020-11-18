c0ab0891a1cd98ef33d1a9d093b51a134313a38d
Gertjan Rolink <32330940+Deedss@users.noreply.github.com>
2020-11-18 20:53:17 +0100

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-pacman -S xorg plasma-meta dolphin konsole spectacle yakuake fwupd pulseaudio pulseaudio-alsa pavucontrol alsa-firmware alsa-lib alsa-plugins alsa-utils gstreamer gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly playerctl volumeicon pulseaudio-bluetooth bluez bluez-libs bluez-utils blueberry cups 
-cups-pdf ghostscript gsfonts gutenprint libcups system-config-printer tlp powertop avahi nss-mdns ark cryfs dolphin-plugins ffmpegthumbs gwenview kde-gtk-config kde-system-meta okular packagekit-qt5 partitionmanager awesome-terminal-fonts adobe-source-sans-pro-fonts cantarell-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font appstream appstream-qt
+pacman -S xorg plasma-meta dolphin konsole spectacle yakuake fwupd pulseaudio pulseaudio-alsa pavucontrol alsa-firmware alsa-lib alsa-plugins alsa-utils gstreamer gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly playerctl volumeicon pulseaudio-bluetooth bluez bluez-libs bluez-utils blueberry cups cups-pdf ghostscript gsfonts libcups system-config-printer tlp powertop avahi nss-mdns ark cryfs dolphin-plugins ffmpegthumbs gwenview kde-gtk-config kde-system-meta okular packagekit-qt5 partitionmanager awesome-terminal-fonts adobe-source-sans-pro-fonts cantarell-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font appstream appstream-qt kcalc kate kdialog kgpg kwalletmanager print-manager kde-system-meta
 
 # Enable display manager
 systemctl enable sddm.service -f
