8b6c870470a65c7d5ed19437fd26191863166edb
Gertjan Rolink <32330940+Deedss@users.noreply.github.com>
2020-11-26 07:48:16 +0100

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -3,7 +3,7 @@
 ####### Base System #######
 pacman -S --needed base-devel yay linux-headers linux-firmware pacman-contrib amd-ucode xdg-utils xdg-users-dirs xf86-video-vesa xorg-server xorg-xinit xf86-input-libinput xf86-video-amdgpu xf86-video-ati xf86-video-fbdev xf86-video-intel xorg-xkill xorg-xrandr xorg-xinput accountsservice smartmontools ldns modemmanager networkmanager networkmanager-openvpn net-tools ntfs-3g nss-mdns usb_modeswitch whois ethtool openconnect openvpn rp-pppoe wireless_tools wpa_supplicant wvdial gnu-netcat iwd linux-atm ndisc6 pptpclient vpnc xl2tpd dialog dnsmasq upower htop python solid mlocate ttf-dejavu gnu-free-fonts ttf-liberation ttf-bitstream-vera ttf-ubuntu-font-family cantarell-fonts noto-fonts noto-fonts-cjk ttf-croscore ttf-carlito ttf-caladea firefox alsa-utils alsa-plugins alsa-firmware glances pulseaudio libopenraw freetype2 gst-libav gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly libdvdcss dosfstools mtools openssh wget bash-completion lsb-release steam tlp inxi adobe-source-sans-pro-fonts
  
-systemctl enable networkmanager
+systemctl enable NetworkManager
 systemctl enable tlp
 systemctl enable avahi-daemon
 
