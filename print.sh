9ceb195f8424cdfc6d6d166866f02ea89a941034
Deedss <gertjan.rolink@outlook.com>
2020-12-02 12:17:36 +0100

    update

--- print.sh
+++ print.sh
@@ -3,4 +3,4 @@
 ####### Printing Setup #######
 pacman -S --needed cups cups-filters cups-pdf ghostscript gsfonts foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds gutenprint foomatic-db-gutenprint-ppds splix
 
-systemctl enable org.cups.cupsd
+systemctl enable cups
