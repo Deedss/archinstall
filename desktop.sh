67296c33f5dca2e2fe6fcdf60e9a870cb7074665
Deedss <gertjan.rolink@outlook.com>
2021-03-13 17:22:50 +0100

    update

--- desktop.sh
+++ desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ####### Desktop Environment #######
-pacman -S --needed plasma sddm fwupd python-dbus discover kde-system-meta kde-utilities-meta gwenview okular spectacle appstream appstream-qt packagekit packagekit-qt5 kdialog dolphin-plugins kcalc kate kdialog ark filelight print-manager yakuake konsole kgpg kfind
+pacman -S --needed plasma sddm fwupd python-dbus discover kde-system-meta gwenview okular spectacle appstream appstream-qt packagekit packagekit-qt5 kdialog dolphin-plugins ark kate kcalc kdialog kfind kgpg konsole print-manager yakuake markdownpart
 
 systemctl enable sddm
